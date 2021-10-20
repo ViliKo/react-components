b75ad1364ca610b16383d8cd082e24e2c1f02812
ViliKo <59921526+ViliKo@users.noreply.github.com>
2021-10-20 22:11:12 +0300

    React components practice

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+let List = () => {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles2</li>
+    </ul>
+  );
+};
+
+export default List;
